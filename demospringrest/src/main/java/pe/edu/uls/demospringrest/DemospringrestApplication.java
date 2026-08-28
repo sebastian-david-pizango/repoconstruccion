3aef40ab847e078fd39b7d63c3dabd186e3637d0
sebastian-david-pizango <sdavidp@ulasalle.edu.pe>
2026-08-28 23:28:44 +0000

    Pending changes exported from your codespace

---
+++ demospringrest/src/main/java/pe/edu/uls/demospringrest/DemospringrestApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.uls.demospringrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemospringrestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemospringrestApplication.class, args);
+	}
+
+}
