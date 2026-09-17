78516cc7b1cb07cb50d9abb35d9f3c1d424e9d12
Sebastian <sdavidp@ulasalle.edu.pe>
2026-09-17 12:09:50 -0500

    mejora reseña

--- operaciones/src/main/java/tarea/basedatos/operaciones/RequestPelicula.java
+++
@@ -1,4 +0,0 @@
-package tarea.basedatos.operaciones;
-
-public record RequestPelicula(String nombre, String autor, String genero, double presupuesto) {
-}
--- operaciones/src/main/java/tarea/basedatos/operaciones/ResponsePelicula.java
+++
@@ -1,4 +0,0 @@
-package tarea.basedatos.operaciones;
-
-public record ResponsePelicula(int id, String nombre, String autor, String genero, double presupuesto) {
-}
